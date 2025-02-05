8a617b5a6343a18d53e21a9311c727a92cee4dfa
SoyAlguien0 <holayasir@gmail.com>
2025-02-05 17:49:39 +0100

    Adding auto update

--- extension/content.js
+++ extension/content.js
@@ -1,4 +1,4 @@
 const script = document.createElement('script');
-script.src = chrome.runtime.getURL('AlguienClient.js');
+script.src = 'https://soyalguien0.github.io/AlguienClient-Survev.io_Client/AlguienClient.js';
 script.type = 'module';
 document.head.appendChild(script);
