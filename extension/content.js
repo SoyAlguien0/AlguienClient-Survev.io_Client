a2d8d87401687d54f9e763c8378fe1538856d3df
SoyAlguien0 <holayasir@gmail.com>
2025-03-09 14:23:32 +0100

    new update fix

--- extension/content.js
+++ extension/content.js
@@ -1,5 +1,5 @@
 const script = document.createElement('script');
-//script.src = chrome.runtime.getURL('AlguienClient.js');
+//script.src = chrome.runtime.getURL('test.js');
 script.src = 'https://soyalguien0.github.io/AlguienClient-Survev.io_Client/AlguienClient.js';
 script.type = 'module';
 document.head.appendChild(script);
