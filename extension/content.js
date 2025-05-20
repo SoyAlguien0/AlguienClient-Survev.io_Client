2fa78d9c7d1c6cd0cc47732082239fa9f0e4a3ad
SoyAlguien0 <holayasir@gmail.com>
2025-05-20 13:00:14 +0200

    New update fix

--- extension/content.js
+++
@@ -1,5 +0,0 @@
-const script = document.createElement('script');
-//script.src = chrome.runtime.getURL('test.js');
-script.src = 'https://soyalguien0.github.io/AlguienClient-Survev.io_Client/AlguienClient.js';
-script.type = 'module';
-document.head.appendChild(script);
